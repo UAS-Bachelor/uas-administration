ca11bf95c11ee77a13f2f373151d1815e0bedcb3
LascariTheMan <lascaritheman@gmail.com>
2018-02-22 11:15:08 +0100

    Login implementeret

--- services/app/app.py
+++ services/app/app.py
@@ -1,10 +1,19 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/login')
+def login():
+    try:
+        login = requests.get('http://127.0.0.1:5002/login').text
+    except requests.exceptions.ConnectionError:
+        return 'Login service unavailable'
+    return login
 
 if __name__ == '__main__':
     app.run(debug=True)
