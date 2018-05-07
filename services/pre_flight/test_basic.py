44ab74376eda69a5725924ac998cca57d484e961
Unknown <lascaritheman@gmail.com>
2018-05-07 21:13:41 +0200

    Init test

---
+++ services/pre_flight/test_basic.py
@@ -0,0 +1,17 @@
+import os
+import unittest
+
+from pre_flight import app
+
+class BasicTests(unittest.TestCase):
+
+    def setUp(self):
+        app.config['TESTING']
+        self.app = app.test_client()
+    
+    def test_main_page(self):
+        response = self.app.get('/new-mission', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+
+if __name__ == "__main__":
+    unittest.main()
